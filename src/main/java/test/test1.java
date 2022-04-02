1e44c8a37740510016f4f388db1bc006430bea05
tom_pro <goodMorning_pro@atguigu.com>
2022-04-02 20:08:50 +0800

    这是主分支上第一次push提交

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -8,5 +8,6 @@ public class test1 {
         System.out.println("这是hos的第一次提交");
         System.out.println("这是主分支上的第四次提交");
         System.out.println("这是hos上第二次提交");
+        System.out.println("这是第一次push提交");
     }
 }
