85070c48aef264d25eb94dfef2b0facfb14f9665
tom_pro <goodMorning_pro@atguigu.com>
2022-04-02 19:43:54 +0800

    这是主分支第二次提交

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -2,6 +2,7 @@ package test;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("");
+        System.out.println("这是主分支第二次提交");
     }
 }
